77d61d1db1d8b7dbfe928e4d6e6a25539ea90a0d
719098 <113333214+719098@users.noreply.github.com>
2026-06-23 14:18:02 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("myself.html")
+
+
+@app.route("/year1")
+def year1():
+    return render_template("year1.html")
+
+
+@app.route("/year2")
+def year2():
+    return render_template("year2.html")
+
+
+@app.route("/year3")
+def year3():
+    return render_template("year3.html")
+
+
+@app.route("/year4")
+def year4():
+    return render_template("year4.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
